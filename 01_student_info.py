ebfd585a6b946a2c6509f563533c98f5698adf93
rakeshdepati-jpg <rakeshdepati@gmail.com>
2026-08-18 12:47:15 +0530

    Add files via upload

---
+++ 01_student_info.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+college = input("Enter your college: ")
+branch = input("Enter your branch: ")
+
+print("My name is", name)
+print("My college is", college)
+print("My branch is", branch)
---
+++ 03_lists.py
@@ -0,0 +1,9 @@
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+print("My fruits:", fruits)
+print("First fruit:", fruits[0])
+print("Second fruit:", fruits[1])
+
+fruits.append("Grapes")
+
+print("After adding a fruit:", fruits)
